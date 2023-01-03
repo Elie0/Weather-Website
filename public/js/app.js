80d895f9a05f959c3414f9d0f71bbf74fb41e030
Elie0 <59401657+Elie0@users.noreply.github.com>
2023-01-03 18:54:56 +0200

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,10 @@ const message2 = document.querySelector('#message-2')
 
 const message3 = document.querySelector('#message-3')
 
+const message4 = document.querySelector('#message-4')
+
+const message5 = document.querySelector('#message-5')
+
 
 weatherForm.addEventListener('submit',(e)=>{       
     e.preventDefault()                      
@@ -15,8 +19,12 @@ weatherForm.addEventListener('submit',(e)=>{
     const input = getUserSearch.value
 
     message3.textContent = 'Fetching Data ...'
+    messageOne.textContent = " "
+    message2.textContent = " "
+    message4.textContent = " "
+    message5.textContent = " "
 
-    fetch('/Weather?adress='+input).then((response)=>{
+    fetch('http://localhost:3000/Weather?adress='+input).then((response)=>{
     response.json().then((data)=>{
 
         if(data.error)
@@ -24,12 +32,17 @@ weatherForm.addEventListener('submit',(e)=>{
               message2.textContent= data.error
               messageOne.textContent = " "
               message3.textContent = " "
+              message4.textContent = " "
+              message5.textContent = " "
         }
         else{
         
             messageOne.textContent = data.location  + " " +  data.forecast
             message2.textContent = " "
             message3.textContent = " "
+            message4.textContent = "wind speed: " + " " + data.windspeed + " miles per hour"
+            message5.textContent = "Pressure: " + " " + data.pressure + " pm"
+
         }
         
     })
